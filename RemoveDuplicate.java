ec76abfab6d8841f3ed826e79d6fff044fbe1ae0
Vaishnavi-Hatture <gaurihatture@5gmail.com>
2026-05-09 19:35:02 +0530

    remove duplicate in-place problem

---
+++ RemoveDuplicate.java
@@ -0,0 +1,19 @@
+public class RemoveDuplicate {
+
+    public int removeDuplicates(int[] nums) {
+      
+       int rd = 0;
+
+       for(int i=0 ; i<nums.length; i++){
+
+            if(nums[i]!=nums[rd]){
+                rd++;
+
+               nums[rd] = nums[i];
+            }
+       }
+
+       return rd+1;
+    }
+}
+
