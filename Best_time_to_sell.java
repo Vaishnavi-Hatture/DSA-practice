ca02f05aef629db97c12eaa01c4cad7638467042
Vaishnavi-Hatture <gaurihatture@5gmail.com>
2026-05-10 23:24:15 +0530

    Best time to sell the stocks problem

---
+++ Best_time_to_sell.java
@@ -0,0 +1,17 @@
+public class Best_time_to_sell {
+   
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int profit = 0;
+
+        for(int i = 1; i < prices.length; i++) {
+            if(prices[i] < min)
+                min = prices[i];
+            else if(prices[i] - min > profit)
+                profit = prices[i] - min;
+        }
+
+        return profit;
+    }
+
+}
