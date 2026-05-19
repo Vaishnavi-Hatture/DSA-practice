92fa60ce52096bf45e23bcb4fa417685acbe627d
Vaishnavi-Hatture <gaurihatture5@gmail.com>
2026-05-19 23:50:29 +0530

    Running Sum of 1d Array problem

---
+++ Running_Sum_of_1d_Array.java
@@ -0,0 +1,13 @@
+public class Running_Sum_of_1d_Array {
+    public int[] runningSum(int[] nums) {
+        int[] arr = new int[nums.length];
+        int sum =0 ;
+        for(int i=0 ; i<nums.length ; i++){
+            
+            sum+=nums[i];
+            arr[i] = sum;
+        } 
+        return arr;
+    }
+
+}
