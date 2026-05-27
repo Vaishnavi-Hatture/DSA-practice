5bc05b8054f927fb3405ab58f62bfc9f3c7f763c
Vaishnavi-Hatture <gaurihatture5@gmail.com>
2026-05-27 17:00:55 +0530

    Baseball Game

---
+++ Baseball_Game.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (String op : operations) {
+
+            if (op.equals("+")) {
+                int size = list.size();
+                list.add(list.get(size - 1) + list.get(size - 2));
+
+            } else if (op.equals("D")) {
+                list.add(list.get(list.size() - 1) * 2);
+
+            } else if (op.equals("C")) {
+                list.remove(list.size() - 1);
+
+            } else {
+                list.add(Integer.parseInt(op));
+            }
+        }
+
+        int sum = 0;
+
+        for (int num : list) {
+            sum += num;
+        }
+
+        return sum;
+    }
+}
