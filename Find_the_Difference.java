c02fc2827a06176d26e2d0f2468d102c61f69108
Vaishnavi-Hatture <gaurihatture5@gmail.com>
2026-06-26 22:51:37 +0530

    Find the Difference problem

---
+++ Find_the_Difference.java
@@ -0,0 +1,16 @@
+class Find_the_Difference {
+    public char findTheDifference(String s, String t) {
+
+        char a = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            a ^= s.charAt(i);
+        }
+
+        for(int i = 0; i < t.length(); i++) {
+            a ^= t.charAt(i);
+        }
+
+        return a;
+    }
+}
