349450b674f533a695a05e13a852616970b055d6
Vaishnavi-Hatture <gaurihatture5@gmail.com>
2026-07-08 22:33:15 +0530

    Student Attendance Record I problem

---
+++ Student_Attendance_Record_I.java
@@ -0,0 +1,24 @@
+class Student_Attendance_Record_I {
+    public boolean checkRecord(String s) {
+        int a = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'A') {
+                a++;
+            }
+
+            if (a >= 2) {
+                return false;
+            }
+
+            if (i + 2 < s.length() &&
+                s.charAt(i) == 'L' &&
+                s.charAt(i + 1) == 'L' &&
+                s.charAt(i + 2) == 'L') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
