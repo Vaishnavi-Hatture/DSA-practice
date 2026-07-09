5390c864dc13819a97a588c99dedac7ff21ba054
Vaishnavi-Hatture <gaurihatture5@gmail.com>
2026-07-09 22:45:42 +0530

    Add to Array-Form of Integer problem

---
+++ Add_to_Array_Form_of_Integer.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Add_to_Array_Form_of_Integer {
+   
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = num.length - 1; i >= 0; i--) {
+            int sum = num[i] + k;
+            ans.add(0, sum % 10);
+            k = sum / 10;
+        }
+
+        while (k > 0) {
+            ans.add(0, k % 10);
+            k /= 10;
+        }
+
+        return ans;
+    }
+}
+
