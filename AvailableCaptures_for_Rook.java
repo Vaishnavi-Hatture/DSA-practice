aad1ee53ab58b43495792ce24194926bf92c0a95
Vaishnavi-Hatture <gaurihatture5@gmail.com>
2026-07-13 21:38:58 +0530

    Available Captures for Rook problem

---
+++ AvailableCaptures_for_Rook.java
@@ -0,0 +1,50 @@
+class Available_Captures_for_Rook {
+    public int numRookCaptures(char[][] board) {
+        int r = 0, c = 0;
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board[i][j] == 'R') {
+                    r = i;
+                    c = j;
+                }
+            }
+        }
+
+        int count = 0;
+
+        for (int i = r - 1; i >= 0; i--) {
+            if (board[i][c] == 'B') break;
+            if (board[i][c] == 'p') {
+                count++;
+                break;
+            }
+        }
+
+        for (int i = r + 1; i < 8; i++) {
+            if (board[i][c] == 'B') break;
+            if (board[i][c] == 'p') {
+                count++;
+                break;
+            }
+        }
+
+        for (int j = c - 1; j >= 0; j--) {
+            if (board[r][j] == 'B') break;
+            if (board[r][j] == 'p') {
+                count++;
+                break;
+            }
+        }
+
+        for (int j = c + 1; j < 8; j++) {
+            if (board[r][j] == 'B') break;
+            if (board[r][j] == 'p') {
+                count++;
+                break;
+            }
+        }
+
+        return count;
+    }
+}
