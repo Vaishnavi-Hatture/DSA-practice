8fcb856fe96048ae2212fcdbbe04bea982d568a7
Vaishnavi-Hatture <gaurihatture5@gmail.com>
2026-07-17 15:55:18 +0530

    Lemonade Change (Greedy Algorithm) problem

---
+++ Lemonade_Change.java
@@ -0,0 +1,28 @@
+public class Lemonade_Change {
+   
+    public boolean lemonadeChange(int[] bills) {
+        int f=0 , t=0;
+        for(int i=0 ; i<bills.length ; i++){
+            if(bills[i]==5)f++;
+            else if(bills[i]==10){
+                if(f>=1){
+                    f--;
+                    t++;
+                }
+                else{
+                    return false;
+                }
+            }else{
+                if(t>=1 && f>=1){
+                    t--;
+                    f--;
+                }else if(f>=3){
+                    f=f-3;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
