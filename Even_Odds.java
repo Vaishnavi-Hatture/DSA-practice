bb1f423f1e962cd96f723f9bf9295eb601135ed0
Vaishnavi-Hatture <gaurihatture5@gmail.com>
2026-08-14 21:43:51 +0530

    Even_Odds codeforces

---
+++ Even_Odds.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Even_Odds {
+    public static void main(String[] args) {
+      
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long k = sc.nextLong();
+
+        long m = (n + 1) / 2; 
+
+        if (k <= m) {
+            System.out.println(k * 2 - 1);
+        } else {
+            k = k - m;
+            System.out.println(k * 2);
+        }
+        sc.close();
+    }
+}
