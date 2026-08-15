9ab731f04b8fd53c6fb5c582d9cbe111ba4a3392
Vaishnavi-Hatture <gaurihatture5@gmail.com>
2026-08-15 13:16:11 +0530

    Number of 1 Bits problem (Bit Manipulation)

---
+++ Number_of_1_Bits.java
@@ -0,0 +1,15 @@
+public class Number_of_1_Bits {
+
+    public int hammingWeight(int n) {
+        String s = Integer.toBinaryString(n);
+        int c=0;
+
+        for(int i=0 ; i<s.length() ; i++){
+            if(s.charAt(i)=='1'){
+                c++;
+            }
+        }
+        return c;
+    }
+
+}
