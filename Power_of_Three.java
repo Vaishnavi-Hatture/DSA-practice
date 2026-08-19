dbd7531b2f897e05bbfdcb89b677a02a9039b589
Vaishnavi-Hatture <gaurihatture5@gmail.com>
2026-08-19 20:54:50 +0530

    Power of Three problem

---
+++ Power_of_Three.java
@@ -0,0 +1,11 @@
+class Power_of_Three {
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0) return false;
+
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+
+        return n == 1;
+    }
+}
