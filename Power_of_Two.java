7663fe6fb0c32b9bdf7c74e35101b888ca31291f
Vaishnavi-Hatture <gaurihatture5@gmail.com>
2026-08-20 20:40:10 +0530

    Power of Two problem

---
+++ Power_of_Two.java
@@ -0,0 +1,13 @@
+public class Power_of_Two {
+  
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+
+        while (n % 2 == 0) {
+            n /= 2;
+        }
+
+        return n == 1;
+    }
+}
+
