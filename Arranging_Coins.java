c70c869cd134f46cea8d2d6335996f814fd5cc98
Vaishnavi-Hatture <gaurihatture5@gmail.com>
2026-08-26 22:02:14 +0530

    Arranging Coins problem

---
+++ Arranging_Coins.java
@@ -0,0 +1,12 @@
+class Arranging_Coins {
+    public int arrangeCoins(int n) {
+        int row = 0;
+
+        while (n >= row + 1) {
+            row++;
+            n -= row;
+        }
+
+        return row;
+    }
+}
