60966f7157da28810630761931de1d705cbd4a71
Vaishnavi-Hatture <gaurihatture5@gmail.com>
2026-09-02 22:18:43 +0530

    Distribute Candies to People problem

---
+++ Distribute_Candies_to_People.java
@@ -0,0 +1,16 @@
+class Distribute_Candies_to_People {
+    public int[] distributeCandies(int candies, int num_people) {
+        int[] ans = new int[num_people];
+        int give = 1;
+        int i = 0;
+
+        while (candies > 0) {
+            ans[i % num_people] += Math.min(give, candies);
+            candies -= give;
+            give++;
+            i++;
+        }
+
+        return ans;
+    }
+}
