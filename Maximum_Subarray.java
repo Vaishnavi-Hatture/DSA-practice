0d8624787cf20d11cb41f1844dad05a950e7c3e4
Vaishnavi-Hatture <gaurihatture5@gmail.com>
2026-09-06 19:26:17 +0530

    Maximum Subarray problem on kadane's algorithm

---
+++ Maximum_Subarray.java
@@ -0,0 +1,17 @@
+public class Maximum_Subarray {
+    
+    public int maxSubArray(int[] nums) {
+       
+         int cs=0 , ms=Integer.MIN_VALUE;
+        for(int i=0 ; i<nums.length ; i++){
+          cs+=nums[i];
+          ms= Math.max(cs,ms);
+          if(cs<0){
+            cs=0;
+          }
+          
+        }
+        return ms; 
+    }
+
+}
