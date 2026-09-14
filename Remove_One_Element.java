df5cb8648cac4ec60b5d622e8ba30d6d2f700d85
Vaishnavi-Hatture <gaurihatture5@gmail.com>
2026-09-14 22:27:25 +0530

    Remove One Element to Make the Array Strictly Increasing problem

---
+++ Remove_One_Element.java
@@ -0,0 +1,25 @@
+public class Remove_One_Element {
+  
+    public boolean canBeIncreasing(int[] nums) {
+        int count = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] <= nums[i - 1]) {
+                count++;
+
+                if (count > 1) {
+                    return false;
+                }
+
+                if (i > 1 && i < nums.length - 1
+                        && nums[i] <= nums[i - 2]
+                        && nums[i + 1] <= nums[i - 1]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+}
