c251b54abf26e21192f9477d19c8e44f6e7b2fa5
Vaishnavi-Hatture <gaurihatture5@gmail.com>
2026-09-17 21:14:11 +0530

    Find the Highest Altitude problem

---
+++ Find_the_Highest_Altitude.java
@@ -0,0 +1,15 @@
+class Find_the_Highest_Altitude {
+    public int largestAltitude(int[] gain) {
+        int altitude = 0;
+        int maxAltitude = 0;
+
+        for (int i = 0; i < gain.length; i++) {
+            altitude += gain[i];
+            if (altitude > maxAltitude) {
+                maxAltitude = altitude;
+            }
+        }
+
+        return maxAltitude;
+    }
+}
