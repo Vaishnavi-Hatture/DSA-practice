b9e47ea2a93ecb9eff97acc090f39f3612511af8
Vaishnavi-Hatture <gaurihatture5@gmail.com>
2026-09-18 23:21:01 +0530

    Subtract the Product and Sum of Digits of an Integer problem

---
+++ Subtract_the_Product.java
@@ -0,0 +1,17 @@
+public class Subtract_the_Product {
+
+    public int subtractProductAndSum(int n) {
+        int product = 1;
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            product *= digit;
+            sum += digit;
+            n /= 10;
+        }
+
+        return product - sum;
+    }
+}
+
