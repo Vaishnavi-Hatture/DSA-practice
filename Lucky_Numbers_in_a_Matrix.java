830f21e2d16f59d33ecfa7c2920bb8d08a76376b
Vaishnavi-Hatture <gaurihatture5@gmail.com>
2026-09-23 23:46:29 +0530

    Lucky Numbers in a Matrix problem

---
+++ Lucky_Numbers_in_a_Matrix.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lucky_Numbers_in_a_Matrix {
+    
+    public List<Integer> luckyNumbers(int[][] matrix) {
+        List<Integer> ans = new ArrayList<>();
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        for (int i = 0; i < m; i++) {
+            int minCol = 0;
+
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] < matrix[i][minCol]) {
+                    minCol = j;
+                }
+            }
+
+            boolean isLucky = true;
+
+            for (int k = 0; k < m; k++) {
+                if (matrix[k][minCol] > matrix[i][minCol]) {
+                    isLucky = false;
+                    break;
+                }
+            }
+
+            if (isLucky) {
+                ans.add(matrix[i][minCol]);
+            }
+        }
+
+        return ans;
+    }
+
+}
